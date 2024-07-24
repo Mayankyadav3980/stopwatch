cac472acd09b8beeecf8b58da1abaf3dd64bbb22
Mayankyadav3980 <ymayank501@gmail.com>
2024-07-24 21:41:54 +0530

    complete index.js

--- index.js
+++ index.js
@@ -1,29 +1,85 @@
+// getting the required html elements
 const startBtn = document.getElementById('start');
 const stopBtn = document.getElementById('stop');
 const resetBtn = document.getElementById('reset');
-const p = document.querySelector('p');
-let flag = true;
 
-startBtn.addEventListener('click', e => {
-    console.log('strt clicked');
-    let i = 1;
-    // let id;
-    while(flag){
-        setInterval(() => {
-            p.textContent = '00:00:' + i;
-            i++;
-        }, 1000);
-    }
+//initializing some variables
+let flag = false;
+let hour = 0;
+let minute = 0;
+let second = 0;
+let count = 0;
+
+// making start, stop, reset button to listen  click events
+startBtn.addEventListener('click', () => {
+  flag = true;
+  startBtn.setAttribute("disabled", "true");
+  stopWatch();
 })
 
 stopBtn.addEventListener("click", (e) => {
-  console.log("stop clicked");
+  startBtn.removeAttribute("disabled");
   flag = false;
 });
 
 resetBtn.addEventListener("click", (e) => {
-  console.log("reset clicked");
-  flag = true;
-  p.innerHTML = '00:00:00';
-
+  startBtn.removeAttribute("disabled");
+  flag = false;
+  hour = 0;
+  minute = 0;
+  second = 0;
+  count = 0;
+  document.getElementById("hr").innerHTML = "00";
+  document.getElementById("min").innerHTML = "00";
+  document.getElementById("sec").innerHTML = "00";
 });
+
+
+// function to handle logic to display the time in stopwatch
+function stopWatch() {
+  if (flag) {
+    count++;
+
+    if (count == 100) {
+      second++;
+      count = 0;
+    }
+
+    if (second == 60) {
+      minute++;
+      second = 0;
+    }
+
+    if (minute == 60) {
+      hour++;
+      minute = 0;
+      second = 0;
+    }
+
+    let hrString = hour;
+    let minString = minute;
+    let secString = second;
+    let countString = count;
+
+    if (hour < 10) {
+      hrString = "0" + hrString;
+    }
+
+    if (minute < 10) {
+      minString = "0" + minString;
+    }
+
+    if (second < 10) {
+      secString = "0" + secString;
+    }
+
+    if (count < 10) {
+      countString = "0" + countString;
+    }
+
+    document.getElementById("hr").innerHTML = hrString;
+    document.getElementById("min").innerHTML = minString;
+    document.getElementById("sec").innerHTML = secString;
+    setTimeout(stopWatch, 10);
+  }
+}
